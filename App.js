73906a5d811df01556380b734c038e24c3ae9604
Jaskaran Singh <42887065+JaskaranSingh4704@users.noreply.github.com>
2021-12-13 16:25:49 +0530

    remove debugger code

--- App.js
+++ App.js
@@ -3,23 +3,6 @@ import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { COLORS } from './src/constants';
 
-
-//Debugger Configuration Start
-// To see all the requests in the chrome Dev tools in the network tab.
-XMLHttpRequest = GLOBAL.originalXMLHttpRequest
-  ? GLOBAL.originalXMLHttpRequest
-  : GLOBAL.XMLHttpRequest;
-
-// fetch logger
-global._fetch = fetch;
-global.fetch = function (uri, options, ...args) {
-  return global._fetch(uri, options, ...args).then(response => {
-    return response;
-  });
-};
-
-//Debugger Configuration End
-
 const App = () => {
 
   // Use Effect hooks
